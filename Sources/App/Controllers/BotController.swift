625a9d907dfb418fe085bca8290f611630b5a75f
Alex Nifontov <nifon1998@yandex.ru>
2021-06-04 18:34:51 +0300

    debug

--- Sources/App/Controllers/BotController.swift
+++ Sources/App/Controllers/BotController.swift
@@ -19,7 +19,7 @@ class BotController {
     
     func handleWebHook(req: Request) throws -> HTTPResponseStatus {
         print(req.description)
-        print()
+        print("")
         print(req.content)
         let webHook = try req.content.decode(Update.self)
         print(webHook.updateType)
