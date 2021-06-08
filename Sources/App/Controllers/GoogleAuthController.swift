187dadf9afadf006cff5fb41eafec967d09d81f1
Alex Nifontov <nifon1998@yandex.ru>
2021-06-08 22:27:45 +0300

    debug

--- Sources/App/Controllers/GoogleAuthController.swift
+++ Sources/App/Controllers/GoogleAuthController.swift
@@ -55,7 +55,7 @@ class GoogleAuthController {
         }
         url.queryItems = items
         
-        url.query = url.query?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+//        url.query = url.query?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
 
         print(url.string!)
         return url.string! // Явное извлечение опционалов плохая практика
