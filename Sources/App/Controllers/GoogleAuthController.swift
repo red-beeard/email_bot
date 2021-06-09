295c1e5fe83862a32ca88b6bbcb52e719bb1fbb2
Alex Nifontov <nifon1998@yandex.ru>
2021-06-09 18:07:53 +0300

    debug

--- Sources/App/Controllers/GoogleAuthController.swift
+++ Sources/App/Controllers/GoogleAuthController.swift
@@ -38,7 +38,7 @@ class GoogleAuthController {
             "client_secret": Environment.googleClientSecret,
             "grant_type": "authorization_code",
             "redirect_uri": redirectUri,
-            "code": authCode,
+            "code": authCode.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
             "access_type": "offline",
             "prompt": "consent"
         ]
