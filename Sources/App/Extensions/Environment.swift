3c8b3d5c63a3cbc8867b4cc21b9757215a7156fc
Alex Nifontov <nifon1998@yandex.ru>
2021-06-14 23:37:22 +0300

    add connect to database

--- Sources/App/Extensions/Environment.swift
+++ Sources/App/Extensions/Environment.swift
@@ -13,4 +13,8 @@ extension Environment {
     static let googleClientId = Self.get("GOOGLE_CLIENT_ID")!
     static let googleProjectId = Self.get("GOOGLE_PROJECT_ID")!
     static let googleClientSecret = Self.get("GOOGLE_CLIENT_SECRET")!
+    
+    static let usernameDatabase = Self.get("USERNAME_DATABASE")!
+    static let passwordDatabase = Self.get("PASSWORD_DATABASE")!
+    static let database = Self.get("DATABASE")!
 }
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,21 @@
 import Vapor
+import Fluent
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.databases.use(.postgres(
+                        hostname: "localhost",
+                        username: Environment.usernameDatabase,
+                        password: Environment.passwordDatabase,
+                        database: Environment.database
+    ), as: .psql)
+    try app.databases.use(.postgres(url: "Connection to database"), as: .psql)
+
 
     // register routes
+    
     try routes(app)
 }
