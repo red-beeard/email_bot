b192efcda3e3b9531ffd636fe3583b1939f009ea
Alex Nifontov <nifon1998@yandex.ru>
2021-06-15 00:18:06 +0300

    add function sendKeyboardChat

--- Sources/App/Controllers/BotController.swift
+++ Sources/App/Controllers/BotController.swift
@@ -48,26 +48,68 @@ class BotController {
         guard let chatId = update.message.recipient.chatId else { return }
         guard let text = update.message.body.text else {
             try sendMessage(with: dontUnderstandMessage, to: chatId)
+            try sendKeyboardChat(to: chatId, and: update.message.sender.userId, for: "email")
             return
         }
         
         if text == "/add" {
-            try sendKeyboard(to: chatId, and: update.message.sender.userId)
+            try sendKeyboardServices(to: chatId, and: update.message.sender.userId)
         } else {
             try sendMessage(with: dontUnderstandMessage, to: chatId)
         }
     }
     
-    private func getKeyboard(for userId: Int64) -> InlineKeyboardAttachmentRequest {
+    private func getKeyboardServices(for userId: Int64) -> InlineKeyboardAttachmentRequest {
         let buttons = [[LinkButton(text: "Google", url: GoogleAuthController().getUrlForAuth(for: userId))]]
         let keyboardPayload = InlineKeyboardAttachmentRequestPayload(buttons: buttons)
         let keyboard = InlineKeyboardAttachmentRequest(payload: keyboardPayload)
         return keyboard
     }
     
-    private func sendKeyboard(to chatId: Int64, and userId: Int64) throws {
-        let keyboard = getKeyboard(for: userId)
+    private func getKeyboardChat(for email: String) -> InlineKeyboardAttachmentRequest {
+        let buttons = [[ChatButton(text: "Open chat", chatTitle: email)]]
+        let keyboardPayload = InlineKeyboardAttachmentRequestPayload(buttons: buttons)
+        let keyboard = InlineKeyboardAttachmentRequest(payload: keyboardPayload)
+        return keyboard
+    }
+    
+    private func sendKeyboardServices(to chatId: Int64, and userId: Int64) throws {
+        let keyboard = getKeyboardServices(for: userId)
         let message = NewMessageBody(with: "Select service:", with: [keyboard])
+        let json = try JSONEncoder().encode(message)
+        
+        guard var url = URLComponents(string: urlMessage) else { return }
+        var items: [URLQueryItem] = []
+        let parameters = ["access_token" : Environment.tamTamToken, "chat_id": String(chatId)]
+        for (key, value) in parameters {
+            items.append(URLQueryItem(name: key, value: value))
+        }
+        url.queryItems = items
+        var request = URLRequest(url: (url.url)!)
+        // Явное извлечения опционала плохая практика
+        request.httpMethod = "POST"
+        request.httpBody = json
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        print("\nОтправляемый запрос:")
+        print(String(data: json, encoding: .utf8) ?? "Не распечатал")
+        print("\n")
+        
+        let session = URLSession(configuration: .ephemeral)
+        session.dataTask(with: request) { (data, response, error) in
+            guard let data = data else { return }
+            if let json = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers),
+               let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .prettyPrinted) {
+                print(String(decoding: jsonData, as: UTF8.self))
+            } else {
+                print("json отсутствует")
+            }
+        }.resume()
+    }
+    
+    private func sendKeyboardChat(to chatId: Int64, and userId: Int64, for email: String) throws {
+        let keyboard = getKeyboardChat(for: email)
+        let message = NewMessageBody(with: "You are signed in for \(email)", with: [keyboard])
         let json = try JSONEncoder().encode(message)
         
         guard var url = URLComponents(string: urlMessage) else { return }
